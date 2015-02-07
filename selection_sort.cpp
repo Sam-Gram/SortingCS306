a9f7e02fc91dba32b66fc6dae02a6692414ff221
Kevin Kopsa <kevin.kopsa@gmail.com>
2015-02-06 17:30:45 -0700

    sort successful with large pseudorandom numbers

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -3,36 +3,31 @@
 #include <cmath>
 using namespace std;
 
-void random(unsigned long long r[], int n, unsigned long long m, unsigned long long seed,
-	 		unsigned long long a, unsigned long long b)
+void random(unsigned long long r[], int n, unsigned long long m,
+	 			unsigned long long seed, unsigned long long a,
+				unsigned long long b)
 {
 	r[0] = seed;
 	for (int i = 1; i <= n; i++)
 		r[i] = (a * r[i-1] + b) % m;
 }
 
-void swap(unsigned long long array[], int pos1, int pos2)
-{
-	unsigned long long value1 = array[pos1];
-	unsigned long long value2 = array[pos2];
-	array[pos1] = value2;
-	array[pos2] = value1;
-	return;
-}
-
 void selectionSort(unsigned long long array[], int n)
 {
 	int min;
+	unsigned long long temp;
 	
-	for (int i = 0; i < n - 2; i++)
+	for (int i = 0; i < n - 1; i++)
 	{
 		min = i;
-		for (int j = i + 1; j <= n - 1; j++)
+		for (int j = i + 1; j < n; j++)
 		{
-			if (array[j] <= array[min])
+			if (array[j] < array[min])
 				min = j;
-			swap(array, i, min);
 		}
+		temp = array[i];
+		array[i] = array[min];
+		array[min] = temp;
 	}
 	return;
 }
@@ -41,7 +36,7 @@ bool checkSorted(unsigned long long array[], int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		if (array[i] > array[i+1])
+		if (array[i] > array[i+1] && i != n - 1)
 			return false;
 	}
 	return true;
@@ -59,7 +54,7 @@ void findMisfits(unsigned long long array[], int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		if (array[i] <= array[i+1])
+		if (array[i] <= array[i+1] || i == n - 1)
 			cout << i << " " << array[i] << endl;
 		else
 			cout << i << " " << array[i] << " MISFIT" << endl;
@@ -81,9 +76,10 @@ int main()
 	if (checkSorted(array, n))
 		cout << "Sort successful." << endl;
 	else
+	{
 		cout << "ERROR: Sort failed." << endl;
-	findMisfits(array, n);
-	
+		findMisfits(array, n);
+	}
 	
 	return 0;
 }
